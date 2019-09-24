5d923b6dbfe36ccb84c5d4f1166c429114f91e6d
Anil Kumar <akchavali9@gmail.com>
2019-09-24 15:46:08 +0200

    Made correction to the error in ascii to morse

--- basic_array_algorithm/include/common.h
+++ basic_array_algorithm/include/common.h
@@ -3,12 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
+void print_msg(char *arr);
 void move_left(int *i,int *j, int *step);
 void move_right(int *i,int *j, int *step);
 
 void ascii_to_morse(char *msg, char *encoder);
 void morse_to_ascii(char *msg, char decoderTree[][31]);
+
+
 
 #endif /* COMMON_H */
--- basic_array_algorithm/src/ascii_to_morse.c
+++ basic_array_algorithm/src/ascii_to_morse.c
@@ -1,11 +1,25 @@
 #include "common.h"
 
+void print_msg(char *msg){
+	int string_end=0;
+	for(int i = 4; i>= 0;i--){
+		if(msg[i] =='0'){
+			i--;
+			string_end =1;
+		}
+		if (string_end == 1 ){
+			printf("%c",msg[i]);
+		}
+	}
+	printf(" ");	
+}
 
 
 void ascii_to_morse(char *msg, char *encoder){
 	printf("Encoded Message: ");
 	while((*msg) != '\0'){
-		int i;	
+		int i,j=0;	
+		char morse_set[5] = {};
 		for(i=0;i<32;i++){
 			if((*(encoder+i)) == (*msg)){
 				break;					
@@ -15,14 +29,17 @@ void ascii_to_morse(char *msg, char *encoder){
 		while (i != 1){
 			if(i%2 == 0){
 				i /= 2;
-				printf(".");
+				morse_set[j]= '.';
 			}else{
 				i = (i-1)/2;
-				printf("-");
+				morse_set[j]= '-';
 			}
+			j++;
 		}
-		printf(" ");
+		morse_set[j]='0';
+		print_msg(morse_set);
 		msg++;
 	}
 	printf("\n");
 }
+
